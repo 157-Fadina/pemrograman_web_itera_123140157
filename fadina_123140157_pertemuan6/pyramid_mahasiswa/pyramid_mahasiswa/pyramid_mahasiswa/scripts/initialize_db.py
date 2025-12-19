dafeb525c5a9be9b0f08550b058ab4b510f0cc18
157-Fadina <fadina.123140157@student.itera.ac.id>
2025-12-19 19:42:35 +0700

    Implement CRUD operations for Mahasiswa model and update database initialization script

--- fadina_123140157_pertemuan6/pyramid_mahasiswa/pyramid_mahasiswa/pyramid_mahasiswa/scripts/initialize_db.py
+++ fadina_123140157_pertemuan6/pyramid_mahasiswa/pyramid_mahasiswa/pyramid_mahasiswa/scripts/initialize_db.py
@@ -1,20 +1,45 @@
 import argparse
 import sys
+from datetime import date
 
 from pyramid.paster import bootstrap, setup_logging
 from sqlalchemy.exc import OperationalError
 
 from .. import models
 
-
 def setup_models(dbsession):
     """
-    Add or update models / fixtures in the database.
-
+    Add initial model objects.
     """
-    model = models.mymodel.MyModel(name='one', value=1)
-    dbsession.add(model)
 
+    existing_mahasiswa1 = dbsession.query(models.Mahasiswa).filter_by(nim='12345').first()
+    existing_mahasiswa2 = dbsession.query(models.Mahasiswa).filter_by(nim='54321').first()
+    
+    if not existing_mahasiswa1:
+        mahasiswa1 = models.Mahasiswa(
+            nim='12345',
+            nama='Budi Santoso',
+            jurusan='Teknik Informatika',
+            tanggal_lahir=date(2000, 5, 15),
+            alamat='Jl. Merdeka No. 123, Bandung'
+        )
+        dbsession.add(mahasiswa1)
+        print("Mahasiswa 12345 added.")
+    else:
+        print("Mahasiswa 12345 already exists.")
+    
+    if not existing_mahasiswa2:
+        mahasiswa2 = models.Mahasiswa(
+            nim='54321',
+            nama='Siti Aminah',
+            jurusan='Sistem Informasi',
+            tanggal_lahir=date(2001, 8, 22),
+            alamat='Jl. Mawar No. 45, Jakarta'
+        )
+        dbsession.add(mahasiswa2)
+        print("Mahasiswa 54321 added.")
+    else:
+        print("Mahasiswa 54321 already exists.")
 
 def parse_args(argv):
     parser = argparse.ArgumentParser()
@@ -28,21 +53,34 @@ def parse_args(argv):
 def main(argv=sys.argv):
     args = parse_args(argv)
     setup_logging(args.config_uri)
+
+    # bootstrap will return a context with request + closer
     env = bootstrap(args.config_uri)
+    request = env['request']
 
     try:
-        with env['request'].tm:
-            dbsession = env['request'].dbsession
+        # gunakan request.tm (bukan tm_manager)
+        from transaction import TransactionManager
+        tm = TransactionManager()
+        dbsession_factory = request.registry['dbsession_factory']
+        with tm:
+            dbsession = models.get_tm_session(dbsession_factory, tm)
             setup_models(dbsession)
+        print("Database initialized successfully.")
+
     except OperationalError:
         print('''
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
+Pyramid is having a problem using your SQL database.
 
-1.  You may need to initialize your database tables with `alembic`.
-    Check your README.txt for description and try to run it.
+Your database should be up and running before you
+initialize your project. Make sure your database server
+is running and your connection string in development.ini
+is correctly configured.
+''')
 
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-            ''')
+    finally:
+        env['closer']()
+
+
+if __name__ == '__main__':
+    main()
